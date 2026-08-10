b5c7e4133ea67981a9f3cf35fa9d15b7b8d882f2
Xenopus19 <82256288+Xenopus19@users.noreply.github.com>
2026-08-10 10:38:29 +0300

    Now chat title is other user's name

--- src/services/chatService.ts
+++ src/services/chatService.ts
@@ -1,6 +1,7 @@
 import { Types } from "mongoose";
 import { ChatMembership } from "../models/ChatMembership";
 import { Chat, type ChatDocument } from "../models/Chat";
+import { User } from "../models/User";
 
 export const getChatById = async (chatId: Types.ObjectId): Promise<ChatDocument | null> => {
     return await Chat.findById(chatId);
@@ -14,7 +15,7 @@ export const createOrFindChatByIds = async (
     if(chat) {
         return chat;
     }
-    const newChat = new Chat({name: `Chat between ${userId1} and ${userId2}`});
+    const newChat = new Chat({});
     await newChat.save();
     const newChatMembership1 = new ChatMembership({ chatId: newChat._id, userId: userId1 });
     const newChatMembership2 = new ChatMembership({ chatId: newChat._id, userId: userId2 });
@@ -27,7 +28,33 @@ export const getUserChats = async (userId: Types.ObjectId | string): Promise<Cha
   const id = typeof userId === "string" ? new Types.ObjectId(userId) : userId;
   const memberships = await ChatMembership.find({ userId: id }).select("chatId");
   const chatIds = memberships.map((membership) => membership.chatId);
-  return Chat.find({ _id: { $in: chatIds } });
+  const chats = (await Chat.find({ _id: { $in: chatIds } })) as ChatDocument[];
+  await Promise.all(
+    chats.map(async (chat) => {
+      if (chat.name) {
+        return;
+      }
+
+      const otherUser = await getOtherUserInChat(chat._id, id);
+      if (!otherUser?.username) {
+        return;
+      }
+
+      chat.name = otherUser.username;
+    }),
+  );
+
+  return chats;
+}
+
+export const getOtherUserInChat = async (chatId: Types.ObjectId, userId: Types.ObjectId) => {
+  const memberships = await ChatMembership.find({ chatId }).select("userId");
+  const otherMembership = memberships.find((membership) => !membership.userId.equals(userId));
+  if(!otherMembership) {
+    return null;
+  }
+  const otherUser = otherMembership ? await User.findById(otherMembership.userId) : null;
+  return otherUser;
 }
 
 export const getChatByUsersIds = async (
