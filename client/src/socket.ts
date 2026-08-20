deabe1f1645fd05ba7d34a713089ac84b35eaf26
Xenopus19 <82256288+Xenopus19@users.noreply.github.com>
2026-08-20 17:45:13 +0300

    Socket types import fixed

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,5 +1,5 @@
 import { io, Socket } from 'socket.io-client';
-import type { ServerToClientEvents, ClientToServerEvents } from '@server-types/socketTypes';
+import type { ServerToClientEvents, ClientToServerEvents } from './socketTypes';
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
   import.meta.env.VITE_SOCKET_URL,
